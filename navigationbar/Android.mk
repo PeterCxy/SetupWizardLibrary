cc5296ce61763c9f550c6177d0b0ff0e019b81c0
Maurice Lam <yukl@google.com>
2014-06-18 20:43:40 -0700

    Add LOCAL_SDK_VERSION to makefile
    Fix build for unbundled branches
    
    Change-Id: Ie2a8381785daa08225c9ca8a3fea64d32eaa8abb

--- navigationbar/Android.mk
+++ navigationbar/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_SDK_VERSION := current
 LOCAL_MODULE := setup-wizard-navbar
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
