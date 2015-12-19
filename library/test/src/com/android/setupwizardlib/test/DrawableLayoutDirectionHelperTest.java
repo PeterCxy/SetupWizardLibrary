38a3832bf55aeb5dc24904d4d1ddf391dffa7d7b
Maurice Lam <yukl@google.com>
2015-12-18 16:48:29 -0800

    [SuwLib] Add preference integration support
    Change-Id: I2e756bcea1512886958ec70c80ae6f4616c9ed81

--- library/test/src/com/android/setupwizardlib/test/DrawableLayoutDirectionHelperTest.java
+++ library/test/src/com/android/setupwizardlib/test/DrawableLayoutDirectionHelperTest.java
@@ -17,6 +17,7 @@
 package com.android.setupwizardlib.test;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
@@ -28,6 +29,8 @@ import android.test.suitebuilder.annotation.SmallTest;
 import android.view.View;
 
 import com.android.setupwizardlib.util.DrawableLayoutDirectionHelper;
+
+import java.util.Locale;
 
 public class DrawableLayoutDirectionHelperTest extends AndroidTestCase {
 
@@ -72,6 +75,33 @@ public class DrawableLayoutDirectionHelperTest extends AndroidTestCase {
         final InsetDrawable insetDrawable =
                 DrawableLayoutDirectionHelper.createRelativeInsetDrawable(drawable,
                         1 /* start */, 2 /* top */, 3 /* end */, 4 /* bottom */, view);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            assertSame("Drawable from getDrawable() should be same as passed in", drawable,
+                    insetDrawable.getDrawable());
+        }
+        Rect outRect = new Rect();
+        insetDrawable.getPadding(outRect);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            assertEquals("InsetDrawable padding should be same as inset", new Rect(3, 2, 1, 4),
+                    outRect);
+        } else {
+            assertEquals("InsetDrawable padding should be same as inset", new Rect(1, 2, 3, 4),
+                    outRect);
+        }
+    }
+
+    @SmallTest
+    public void testCreateRelativeInsetDrawableContextRtl() {
+        final Drawable drawable = new ColorDrawable(Color.RED);
+        Context context = getContext();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            final Configuration config = new Configuration();
+            config.setLayoutDirection(new Locale("fa", "IR"));
+            context = getContext().createConfigurationContext(config);
+        }
+        final InsetDrawable insetDrawable =
+                DrawableLayoutDirectionHelper.createRelativeInsetDrawable(drawable,
+                        1 /* start */, 2 /* top */, 3 /* end */, 4 /* bottom */, context);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             assertSame("Drawable from getDrawable() should be same as passed in", drawable,
                     insetDrawable.getDrawable());
