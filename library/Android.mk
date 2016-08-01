3771a653ed5ea7c1e21b70111825f23b9fe77aa1
Adam Lesinski <adamlesinski@google.com>
2016-08-01 16:13:52 -0700

    Build with AAPT2
    Change-Id: Id48c7095055860898a2ab75fea3ea83b723b3b92

--- library/Android.mk
+++ library/Android.mk
@@ -5,7 +5,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_USE_AAPT2 := true
 LOCAL_MANIFEST_FILE := main/AndroidManifest.xml
 LOCAL_MODULE := setup-wizard-lib
 LOCAL_RESOURCE_DIR := \
@@ -23,17 +23,15 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.appcompat
+LOCAL_USE_AAPT2 := true
 LOCAL_MANIFEST_FILE := main/AndroidManifest.xml
 LOCAL_MODULE := setup-wizard-lib-eclair-mr1-compat
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/main/res \
-    $(LOCAL_PATH)/eclair-mr1/res \
-    frameworks/support/v7/appcompat/res
+    $(LOCAL_PATH)/eclair-mr1/res
 LOCAL_SDK_VERSION := current
 LOCAL_SRC_FILES := $(call all-java-files-under, main/src eclair-mr1/src)
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat
 
@@ -46,20 +44,16 @@ include $(BUILD_STATIC_JAVA_LIBRARY)
 #
 include $(CLEAR_VARS)
 
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.recyclerview
+LOCAL_USE_AAPT2 := true
 LOCAL_MANIFEST_FILE := main/AndroidManifest.xml
 LOCAL_MODULE := setup-wizard-lib-full-support
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/main/res \
     $(LOCAL_PATH)/eclair-mr1/res \
-    $(LOCAL_PATH)/full-support/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res
+    $(LOCAL_PATH)/full-support/res
 LOCAL_SDK_VERSION := current
 LOCAL_SRC_FILES := $(call all-java-files-under, main/src eclair-mr1/src full-support/src)
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_SHARED_ANDROID_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
     android-support-v7-recyclerview
