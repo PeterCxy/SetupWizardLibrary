66815fe66392bcbb12e2fc93bbf326a5c2d8782f
Maurice Lam <yukl@google.com>
2017-12-13 02:51:32 +0000

    Revert "Revert "Changes needed to upgrade to new gradle version""
    This reverts commit 46216d0e61318e7af168e9e903ef8d943bb49b2e.
    
    Reason for revert: Roll forward
    - Added ag/3351790 to the topic
    
    Change-Id: I64990e558d3f77165ab3fe89006a02225b712257

--- library/gingerbread/src/com/android/setupwizardlib/view/NavigationBarButton.java
+++ library/gingerbread/src/com/android/setupwizardlib/view/NavigationBarButton.java
@@ -16,6 +16,7 @@
 
 package com.android.setupwizardlib.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.PorterDuff;
@@ -30,6 +31,7 @@ import android.widget.Button;
  * Button for navigation bar, which includes tinting of its compound drawables to be used for dark
  * and light themes.
  */
+@SuppressLint("AppCompatCustomView")
 public class NavigationBarButton extends Button {
 
     public NavigationBarButton(Context context) {
